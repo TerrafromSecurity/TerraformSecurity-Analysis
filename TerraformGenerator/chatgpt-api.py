0bac7928321fe48bab556f783ade7536bb6c9529
Felix Sandmair <felix.sandmair@tum.de>
2023-01-19 15:26:19 +0100

    final changes not tested

--- TerraformGenerator/chatgpt-api.py
+++ TerraformGenerator/chatgpt-api.py
@@ -6,6 +6,7 @@ from chatgpt_wrapper import ChatGPT
 import json
 import os
 from pypandoc.pandoc_download import download_pandoc
+import numpy as np
 
 # download_pandoc()
 
@@ -84,12 +85,15 @@ def run_tfSec():
 # Analyse tfSec json output
 def analyse_tfSec_output(output):
     issues: set = set()
-    for issue in output["results"]:
-        issues.add(json.dumps(issue))
-    return len(issues), issues
+    if output["results"] == None:
+        return 0, issues
+    else:
+        for issue in output["results"]:
+            issues.add(json.dumps(issue))
+        return len(issues), issues
 
 # Wrap up
-def finish(code: str, data):
+def finish(code: str, data, id: int):
     # save final TF file
     f = open(f"prompts/prompt_{id}_final.tf", "w+")
     f.write(code)
@@ -101,100 +105,130 @@ def finish(code: str, data):
 
 # Execute the chatGPT runner for a single prompt
 def createPrompt(inputPrompt):
-    # Stores all the generated code blocks
-    data = []
-    id, prompt = inputPrompt.values()
 
-    # (0) for the first question to chatgpt we add some setting prompts
-    prompt = settings + "\n" + prompt
-
-    # (1) Get first response and save to file
-    if debug:
-        print(f"### PROMPT: {prompt}")
-        print("Waiting for response ... ")
-
-    response = bot.ask(prompt)
+    #These variables store the solution of the five attempts
+    codeOfAttempts = []
+    numOfVulnerabilities = []
+    vulnerabilities = []
     
-    if debug:
-        print("ChatGPT: \n", response)
+    # This loop makes 5 attempts at solving all security vulnerabilities (or less if we had one attempt that succeeded)
+    for i in range(5):
+        # Stores all the generated code blocks
+        data = []
+        id, prompt = inputPrompt.values()
 
-    # (2) Extract code from markdown file and write to terraform file
-    code = parse_response(response)
+        # (0) for the first question to chatgpt we add some setting prompts
+        prompt = settings + "\n" + prompt
 
-    # (3) run tfsec on the terraform file
-    # (4) extract tfsec descriptions from the json output
-    tfSecOutput = run_tfSec()
+        # (1) Get first response and save to file
+        if debug:
+            print(f"### PROMPT: {prompt}")
+            print("Waiting for response ... ")
 
-    # (5) check if tfsec found any issues
-    number_of_issues, issuesToProcess = analyse_tfSec_output(tfSecOutput)
+        response = bot.ask(prompt)
+        
+        if debug:
+            print("ChatGPT: \n", response)
 
-    # Append initial prompt to data
-    data.append({
-            "prompt": str(prompt),
-            "code": str(code),
-            "tfsec": str(tfSecOutput),
-            "number_of_issues": str(number_of_issues)
-        })
+        # (2) Extract code from markdown file and write to terraform file
+        code = parse_response(response)
 
-    processed_issues: set = set()
-    resolved_issues: set = set()
-    # (6) Run loop over all found issues and resolve issue by issue
-    while True:
-        # if issues set is empty we have tried to resolve all issues that we found
-        if not issuesToProcess:
+        # (3) run tfsec on the terraform file
+        # (4) extract tfsec descriptions from the json output
+        tfSecOutput = run_tfSec()
+
+        # (5) check if tfsec found any issues
+        number_of_issues, issuesToProcess = analyse_tfSec_output(tfSecOutput)
+
+        # Append initial prompt to data
+        data.append({
+                "prompt": str(prompt),
+                "code": str(code),
+                "tfsec": str(tfSecOutput),
+                "number_of_issues": str(number_of_issues)
+            })
+
+        processed_issues: set = set()
+        resolved_issues: set = set()
+        # (6) Run loop over all found issues and resolve issue by issue
+        while True:
+            # if issues set is empty we have tried to resolve all issues that we found
+            if not issuesToProcess:
+                break
+
+            issue = issuesToProcess.pop()
+
+            if debug:
+                print("issue: " + issue)
+
+            jsonIssue = json.loads(issue)
+
+            processed_issues.add(jsonIssue["rule_id"])
+            prompt = code + "\n" + "My code has the following security vulnerability. Can you fix this and " \
+                                            "print out the full code? " + issue
+                                            
+            # Rerun the same request as long as the same issue persists in the code and no new issues have been introduced
+            for attempt in range(3):
+                response = bot.ask(prompt)
+                codeTmp = parse_response(response)
+                tfSecOutputTmp = run_tfSec()
+                number_of_issues, issues = analyse_tfSec_output(tfSecOutput)
+                issueIds: set = set()
+                jsonIssues = json.loads(issues)
+                for i in issues:
+                    issueIds.add(i["rule_id"])
+                if len(issueIds.intersection(resolved_issues)) == 0 and jsonIssue["rule_id"] not in issueIds:
+                    resolved_issues.add(jsonIssue["rule_id"])
+                    tfSecOutput = tfSecOutputTmp
+                    code = codeTmp
+                    for newIssue in jsonIssues:
+                        if newIssue["rule_id"] not in processed_issues:
+                            issuesToProcess.add(newIssue["rule_id"])
+                    break
+
+            data.append({
+                "prompt": str(prompt),
+                "code": str(code),
+                "tfsec": str(tfSecOutput),
+                "number_of_issues": str(number_of_issues)
+            })
+
+        # (5b) save all the data of this iteration to a json file
+        codeOfAttempts.append(code)
+        numOfVulnerabilities.append(number_of_issues)
+        vulns = []
+        for issue in jsonIssue:
+            vulns.append(issue["rule_id"])
+        vulnerabilities.append(vulns)
+        finish(code, data, id)
+
+        if number_of_issues == 0:
             break
 
-        resolved: bool = True
-        issue = issuesToProcess.pop()
 
-        if debug:
-            print("issue: " + issue)
+    bestAttempt = np.argmin(numOfVulnerabilities)
+    code = codeOfAttempts[bestAttempt]
+    print("Here is a suggestion for a Tf script with the following description: " + prompt)
+    print(code)
+    print("There are " + str(np.min(numOfVulnerabilities)) + " security vulnerabilities left in this script.")
+    for i, vuln in enumerate(vulnerabilities[bestAttempt]):
+        print(f"Vulnerability {i}: {vuln}")
 
-        jsonIssue = json.loads(issue)
 
-        processed_issues.add(jsonIssue["rule_id"])
-        prompt = code + "\n" + "My code has the following security vulnerability. Can you fix this and " \
-                                          "print out the full code? " + issue
-                                        
-        # Rerun the same request as long as the same issue persists in the code and no new issues have been introduced
-        for attempt in range(3):
-            response = bot.ask(prompt)
-            code = parse_response(response)
-            tfSecOutput = run_tfSec()
-            number_of_issues, issues = analyse_tfSec_output(tfSecOutput)
-            issueIds: set = set()
-            jsonIssues = json.loads(issues)
-            for i in issues:
-                issueIds.add(jsonIssues["rule_id"])
-            if len(issueIds.intersection(resolved_issues)) == 0 and jsonIssue["rule_id"] not in issueIds:
-                resolved = True
-                for newIssue in jsonIssues:
-                    if newIssue["rule_id"] not in processed_issues:
-                        issuesToProcess.add(newIssue["rule_id"])
-                break
-        
-        # if issue was resoleved add it to resolved issues
-        if resolved:
-            resolved = False
-            resolved_issues.add(issue["rule_id"])
-
-        data.append({
-            "prompt": str(prompt),
-            "code": str(code),
-            "tfsec": str(tfSecOutput),
-            "number_of_issues": str(number_of_issues)
-        })
-
-    # (5b) save all the data of this iteration to a json file
-
-    finish(code, data)
 
 
 
 debug = True
 bot = ChatGPT()
-settings = " ".join(settingsPrompts)
-init()
 
-for p in prompts:
-    createPrompt(p)
+#test if ChatGPT bot is working
+
+reponse = bot.ask("Hello")
+if not (reponse == 'Your ChatGPT session is not usable.\n* Run this program with the `install` parameter and log in to ChatGPT.\n* If you think you are already logged in, try running the `session` command.'):
+    settings = " ".join(settingsPrompts)
+    init()
+
+    for p in prompts:
+        createPrompt(p)
+else:
+    print("ChatGPT is not working.")
--- TerraformGenerator/tmp/chatgpt.md
+++ TerraformGenerator/tmp/chatgpt.md
@@ -1,30 +1,3 @@
-Here is an example of Terraform configuration code that deploys an AWS EC2 instance with the root block device encrypted:
-
-```
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.26.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-west-2"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
-  root_block_device {
-    volume_type           = "gp2"
-    encrypted             = true
-    delete_on_termination = true
-  }
-}
-```
-
-Note: You can use different volume_type and encryption algorithm as per your requirement.
-
-In this example, the `root_block_device` block has been added to the `aws_instance` resource and the `encrypted` parameter is set to true. This will encrypt the root block device of the EC2 instance at rest. The `delete_on_termination` parameter is set to true so that the volume will be deleted when the instance is terminated. 
+Your ChatGPT session is not usable.
+* Run this program with the `install` parameter and log in to ChatGPT.
+* If you think you are already logged in, try running the `session` command.
