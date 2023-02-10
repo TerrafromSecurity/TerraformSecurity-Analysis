0d12b9b95bb3fcfa41b5fee1e9bf07af7fb767e7
Elias Berger <elias.berger@kertos.io>
2023-02-10 09:32:00 +0100

    scripts and data

--- analysis_script/issue_per_file.py
+++ analysis_script/issue_per_file.py
@@ -1,12 +1,26 @@
 import pandas as pd
+import plotly.express as px
+
+
+# compute the mean and std dev of the number of issues per file
 
 df = pd.read_csv("../data/security_smells_AWS_ChatGPT.csv")
+df["issues"] = 1
 number_of_issues = len(df)
-number_of_files_affected = 376
-number_of_issues_per_file = number_of_issues / number_of_files_affected
-print(f"Number of issues: {number_of_issues}")
-print(f"Number of issues per file: {number_of_issues_per_file}")
+df = df.groupby(["file"]).sum().reset_index()
+
+mu = df["issues"].mean()
+std_deviation = df["issues"].std()
+print("mean", mu)
+print("std_deviation", std_deviation)
 
 
-# ChatGPT 11.590425531914894
-# Codex 3.504
+px.histogram(df, x="issues")
+
+# non optimized chat gpt
+# mean 4.3243967828418235
+# std_deviation 3.997898637979647
+
+# optimized chat gpt
+# mean 3.3095238095238093
+# std_deviation 3.0562857678138067
--- analysis_script/optimized_chatgpt_number_issues_dist.py
+++ analysis_script/optimized_chatgpt_number_issues_dist.py
@@ -4,8 +4,8 @@ import pandas as pd
 # get all json files that store our interactions with chatgpt
 # these are the final, optimized files
 json_files_final_run = [
-    os.path.join(dp, f) for dp, dn, fn in os.walk("../data/prompts_new_format") for f in fn
-    if ("final" in f) and f.endswith(".json")]
+    os.path.join(dp, f) for dp, dn, fn in os.walk("../data/prompts") for f in fn
+    if ("final" in f or "try_4" in f) and f.endswith(".json")]
 
 total_files = len(json_files_final_run)
 
@@ -15,11 +15,9 @@ for filename in json_files_final_run:
     df = pd.read_json(filename)
     li.append(df)
 frame_all_runs = pd.concat(li, axis=0, ignore_index=True)
-frame_all_runs["number_of_issues"].astype(int)
 
 # count number of columns where number_of_issues is 0
-number_of_files_unaffected = len(frame_all_runs[frame_all_runs["number_of_issues"] > 0])
+number_of_files_unaffected = len(frame_all_runs[frame_all_runs["number_of_issues"] == 0])
 
-print(f"Number of files affected: {number_of_files_unaffected}")
+print(f"Number of files affected: {total_files- number_of_files_unaffected}")
 print(f"Total number of files: {total_files}")
-
