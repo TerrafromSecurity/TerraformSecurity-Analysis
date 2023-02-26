04445da67c00732dcb9a800e0f60fcb3cc3d06b7
Elias Berger <elias.berger@kertos.io>
2023-02-26 15:51:58 +0100

    remove old prompts

--- data/prompts_not_used/prompt8/versuch5/prompt_8_final_2.tf
+++
@@ -1,9 +0,0 @@
-metadata_options {
-    http_token = "required"
-  }
-
-root_block_device {
-    volume_encryption {
-      enabled = true
-    }
-  }
--- data/prompts_not_used/prompt_2_versuch_1/prompt_2_final_3.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_kms_key" "example" {
-  description = "key for encrypting cloudwatch logs"
-  enable_key_rotation = true
-}
